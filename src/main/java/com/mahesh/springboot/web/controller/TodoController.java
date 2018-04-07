0c05d598c073bd2fb932efcbdb2751dbb6bd93a5
Mahesh Karthik Duraisamy <maheshkarthikduraisamy@Maheshs-Mac-mini.local>
2018-04-06 23:52:30 -0400

    Bootstrap introduced here

--- src/main/java/com/mahesh/springboot/web/controller/TodoController.java
+++ src/main/java/com/mahesh/springboot/web/controller/TodoController.java
@@ -8,8 +8,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.Date;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
 
     @Autowired
@@ -18,8 +22,22 @@ public class TodoController {
     @RequestMapping(value="/list-todos",method=RequestMethod.GET)
     public String showTodosList(ModelMap model)
     {
-        model.put("todos",service.retrieveTodos("mahesh"));
+        String name = (String) model.get("name");
+        model.put("todos",service.retrieveTodos(name));
         return "list-todos" ;
+    }
+
+    @RequestMapping(value="/add-todo",method=RequestMethod.GET)
+    public String showAddTodo(ModelMap model)
+    {
+        return "todo" ;
+    }
+
+    @RequestMapping(value="/add-todo",method=RequestMethod.POST)
+    public String addTodo(ModelMap model,@RequestParam String desc)
+    {
+        service.addTodo((String) model.get("name"),desc,new Date(),false);
+        return "redirect:/list-todos" ;
     }
 
 
---
+++ src/main/webapp/WEB-INF/jsp/todo.jsp
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+<title>Add/Update Todo</title>
+</head>
+
+<body>
+  Add Todo Page for ${name}
+
+  <form method="post">
+  	Description : <input name="desc" type="text"/>
+  	<input type="submit"/>
+
+
+</html>
